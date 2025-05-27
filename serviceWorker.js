12d8a7a763f04c3bb671b2c73a262d9448af15cb
pd2 <its.pdp@gmail.com>
2025-05-27 11:46:31 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,9 +2,9 @@ const staticDev = "panchang_puzzle"
 const service_worker_version = "v1.0103"
 const assets = [
   "/",
-  "/index.html",
-  "/style.css",
-  "/script.js"
+  "index.html",
+  "style.css",
+  "script.js"
 ]
 
 self.addEventListener("install", installEvent => {
